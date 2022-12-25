8f3b305b1576b8ae29d1b5bcb1248f994dbd2f32
KaushalTarpara <95602932+KaushalTarpara@users.noreply.github.com>
2022-12-25 15:55:50 +0000

    Day2_variables

---
+++ Basics/01_variables.js
@@ -0,0 +1,14 @@
+const userID=101            //Can't Change
+let userName="Kaushal"
+var userEmail="KdTarpara@code.com" //Prefer not to Use var Because f issue  in block 
+userCountry="India"
+
+console.log(userID);
+console.table([userID,userName,userEmail,userCountry])
+
+
+userName="KDTarpara"
+userEmail="KDcoder@code.com"
+userCountry="Japan"
+
+console.table([userID,userName,userEmail,userCountry])
