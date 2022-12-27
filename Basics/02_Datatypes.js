f27697f45f2731994daa579a192d15e9f8d677e2
Kaushal Tarpara <95602932+KaushalTarpara@users.noreply.github.com>
2022-12-27 16:09:00 +0000

    Day3_DataType

---
+++ Basics/02_Datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //use Node JS,not  Browser
+
+// Number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof typeof 1);
+console.log(typeof "Kaushal");   //String
+console.log(typeof null);        //object
+console.log(typeof undefined)    //undefined
