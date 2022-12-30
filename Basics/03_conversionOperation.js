7accb5d22713f35d80baf2b469df55ff98274cad
Kaushal Tarpara <95602932+KaushalTarpara@users.noreply.github.com>
2022-12-30 17:12:28 +0000

    DataType Conversion

---
+++ Basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score=true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//-----Number Conversion------
+let stringNumber=Number(score);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+
+// "33" => 33
+// "33abc" => NaN(Not a Number)
+// true => 1 false=> 0
+
+
+//-----------Boolean Converison------------
+let isLoggedIn=1
+
+let boolLoggedIn=Boolean(isLoggedIn)
+console.log(boolLoggedIn);
+
+// 1 =>true
+// "Kauhsal" =>true
+// ""=>false
+
+//--String Conver-----
+
+let mark=""
+let stringMark=String(mark);
+console.log(typeof stringMark)
+console.log(stringMark);
